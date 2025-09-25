b6c80ac0de5cf80d001f5f5e0de275c9f1e250fc
tlsrltjq <psl87531@naver.com>
2025-09-25 10:10:48 +0900

    feat: 사용자 입력시 유효성 검사 및 재입력 로직 추가 & 기존 변수와 메서드 이름 변경

--- src/CommerceSystem.java
+++ src/CommerceSystem.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -17,14 +18,14 @@ public class CommerceSystem {
         ele.getProducts().add(new Product("AirPods Pro", "350,000원", "노이즈 캔슬링 무선 이어폰", 5));
         foods.getProducts().add(new Product("사과", "1,200원", "유기농 사과", 10));
         foods.getProducts().add(new Product("바나나", "2,000원", "필리핀 바나나", 3));
-        clothes.getProducts().add(new Product("디자인티셔츠", "20,000원", "토마토그림이있는 티셔츠", 10));
+        clothes.getProducts().add(new Product("디자인티셔츠", "20,000원", "토마토 그림이 있는 티셔츠", 10));
 
         categories.add(ele);
         categories.add(foods);
         categories.add(clothes);
     }
 
-    void start() {
+    public void start() {
         while (true) {
             int i = 0;
             System.out.println("[ 실시간 커머스 플랫폼 메인 ]");
@@ -32,41 +33,33 @@ public class CommerceSystem {
                 System.out.println((i + 1) + ". " + c.getName());
                 i++;
             }
-
             System.out.println("0. 종료   |   프로그램 종료");
-            String next = sc.nextLine();
-            int pNum = Integer.parseInt(next);
 
-            if (next.equals("0")) {
+            int pNum = firstInput();
+            if (pNum == 0) {
                 System.out.println("커머스 플랫폼을 종료합니다.");
                 sc.close();
                 System.exit(0);
             } else {
                 switch (pNum) {
                     case 1:
-                        categoryList(pNum - 1);
-                        int pNum2 = numberInput();
+                        int index = pNum - 1;
+                        productList(index);
+                        int pNum2 = secondInput();
 
                         if (pNum2 == 0) continue;
-                        else productList(categories.get(pNum - 1), pNum2);
-
+                        else selectProduct(categories.get(index), pNum2);
                         break;
-                    case 2:
-                        categoryList(pNum - 1);
-                        pNum2 = numberInput();
+
+                    case 2, 3:
+                        index = pNum - 1;
+                        productList(index);
+                        pNum2 = secondInput();
 
                         if (pNum2 == 0) continue;
-                        else productList(categories.get(pNum - 1), pNum2);
-
+                        else selectProduct(categories.get(index), pNum2);
                         break;
-                    case 3:
-                        categoryList(pNum - 1);
-                        pNum2 = numberInput();
 
-                        if (pNum2 == 0) continue;
-                        else productList(categories.get(pNum - 1), pNum2);
-
-                        break;
                     default:
                         System.out.println("보기에 있는 숫자 중에 눌러주세요");
                 }
@@ -74,7 +67,7 @@ public class CommerceSystem {
         }
     }
 
-    void categoryList(int i) {
+    public void productList(int i) {
         System.out.println("[ " + categories.get(i).categoryName + " 카테고리 ]");
         int j = 1;
         for (Product p : categories.get(i).getProducts()) {
@@ -84,17 +77,42 @@ public class CommerceSystem {
         System.out.println("0. 뒤로가기");
     }
 
-    void productList(Category c, int pNum2) {
-        pNum2 -= 1;
-        Product select = c.getProducts().get(pNum2);
+    public void selectProduct(Category category, int pNum2) {
+        int index = pNum2 - 1;
+        int listSize = category.getProducts().size();
+
+        if (index < 0 || index >= listSize) {
+            System.err.println("잘못된 상품 번호입니다. " + category.getName() + " 카테고리에는 " + listSize + "개의 상품이 있습니다.");
+            return;
+        }
+        Product select = category.getProducts().get(index);
         System.out.println("선택한 상품 : " + select + " | 남은 재고: " + select.getStock() + "개");
         System.out.println("");
     }
 
-    int numberInput() {
-        int pNum2 = sc.nextInt();
-        sc.nextLine();
+    public int firstInput() {
+        try {
+            int pNum = sc.nextInt();
+            sc.nextLine();
+            return pNum;
+        } catch (InputMismatchException e) {
+            System.err.println("숫자만 입력해야 합니다. 잘못된 입력입니다.");
+            sc.nextLine();
+            return firstInput();
+        }
+    }
 
-        return pNum2;
+    public int secondInput() {
+        while (true) {
+            try {
+                int pNum2 = sc.nextInt();
+                sc.nextLine();
+                return pNum2;
+            } catch (InputMismatchException e) {
+                System.err.println("숫자만 입력해야 합니다. 잘못된 입력입니다.");
+                sc.nextLine();
+                return secondInput();
+            }
+        }
     }
 }
